54a9e310be40ca06fdf20f04899086a1397284df
huben1337 <88612147+huben1337@users.noreply.github.com>
2024-12-20 20:59:44 +0100

    Add subline codegen

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -10,7 +10,7 @@ namespace codegen {
 constexpr uint8_t CODE_INDENT_SPACES = 4;
 
 template <uint8_t indent, size_t N>
-INLINE void _add (const char (&line)[N], Buffer &buffer) {
+INLINE void _line (const char (&line)[N], Buffer &buffer) {
     constexpr uint16_t indent_size = indent * 4;
     constexpr size_t line_size = N - 1;
     constexpr size_t str_size = indent_size + line_size + 1;
@@ -23,7 +23,7 @@ INLINE void _add (const char (&line)[N], Buffer &buffer) {
 }
 
 template <uint8_t indent>
-INLINE void _add (std::string line, Buffer &buffer) {
+INLINE void _line (std::string line, Buffer &buffer) {
     constexpr uint16_t indent_size = indent * 4;
     size_t line_size = line.size();
     size_t str_size = indent_size + line_size + 1;
@@ -37,7 +37,7 @@ INLINE void _add (std::string line, Buffer &buffer) {
 
 
 
-#define ADD(INDENT, LINE) _add<INDENT>(LINE, this->buffer)
+#define LINE(INDENT, LINE) _line<INDENT>(LINE, this->buffer)
 
 
 
@@ -60,6 +60,8 @@ struct ClosedCodeBlock {
     
 };
 
+
+
 template <size_t indent, typename Last, typename Derived>
 struct __CodeBlock : CodeData {
     using __Last = Last;
@@ -69,8 +71,10 @@ struct __CodeBlock : CodeData {
     INLINE auto _switch (std::string key);
     INLINE auto _struct (std::string name);
 
-    INLINE Derived add (std::string line) {
-        ADD(indent, line);
+    INLINE auto operator() (std::string value);
+
+    INLINE Derived line (std::string line) {
+        LINE(indent, line);
         return c_cast<Derived>(__CodeBlock<indent, Last, Derived>({this->buffer}));
     }
 
@@ -78,12 +82,51 @@ struct __CodeBlock : CodeData {
         if constexpr (std::is_same_v<Last, ClosedCodeBlock>) {
             return ClosedCodeBlock(this->buffer);
         } else {
-            ADD(indent - 1, "}");
+            LINE(indent - 1, "}");
             return c_cast<Last>(__CodeBlock<indent - 1, typename Last::__Last, typename Last::__Derived>({this->buffer}));
         }
     }
 };
 
+template <size_t indent, typename Last>
+struct Line : CodeData {
+    INLINE Line (Buffer buffer) : CodeData(buffer) {}
+    /**
+     * Concatenate the given string to the line of code stored in this object
+     * and return a new Line object with the concatenated line.
+     *
+     * @param value the string to concatenate
+     * @return a new Line object
+     */
+    INLINE auto operator() (std::string value) {
+        size_t size = value.size();
+        auto str = buffer.get(buffer.get_next_multi_byte<char>(size));
+        std::memcpy(str, value.c_str(), size);
+        return Line<indent, Last>(this->buffer);
+    }
+    /**
+     * Write the line of code stored in this object to the code buffer, and return
+     * a CodeBlock that can be used to generate more code.
+     *
+     * @return a CodeBlock
+     */
+    INLINE auto nl() {
+        *buffer.get_next<char>() = '\n';
+        return c_cast<Last>(__CodeBlock<indent, typename Last::__Last, typename Last::__Derived>({this->buffer}));
+    };
+};
+template <size_t indent, typename Last, typename Derived>
+INLINE auto __CodeBlock<indent, Last, Derived>::operator() (std::string value) {
+    constexpr uint16_t indent_size = indent * 4;
+    size_t size = value.size();
+    size_t str_size = indent_size + size;
+    auto str = buffer.get(buffer.get_next_multi_byte<char>(str_size));
+    for (uint16_t i = 0; i < indent_size; i++) {
+        str[i] = ' ';
+    }
+    std::memcpy(str + indent_size, value.c_str(), size);
+    return Line<indent, Derived>(this->buffer);
+}
 
 template <size_t indent, typename Last>
 struct CodeBlock : __CodeBlock<indent, Last, CodeBlock<indent, Last>> {};
@@ -92,13 +135,13 @@ struct CodeBlock : __CodeBlock<indent, Last, CodeBlock<indent, Last>> {};
 template <size_t indent, typename Last>
 struct If : __CodeBlock<indent, Last, If<indent, Last>> {
     INLINE auto _else () {
-        ADD(indent - 1, "} else {");
+        LINE(indent - 1, "} else {");
         return CodeBlock<indent, Last>({{this->buffer}});
     }
 };
 template <size_t indent, typename Last, typename Derived>
 INLINE auto __CodeBlock<indent, Last, Derived>::_if (std::string condition) {
-    ADD(indent, "if (" + condition + ") {");
+    LINE(indent, "if (" + condition + ") {");
     return If<indent + 1, Derived>({{this->buffer}});
 };
 
@@ -112,28 +155,28 @@ struct Case : __CodeBlock<indent, Last, Case<indent, Last>> {
 template <size_t indent, typename Last>
 struct Switch : CodeData {
     INLINE auto _case(std::string value) {
-        ADD(indent, "case " + value + ": {");
+        LINE(indent, "case " + value + ": {");
         return Case<indent + 1, Last>({{buffer}});
     };
 
     INLINE auto _default () {
-        ADD(indent, "default: {");
+        LINE(indent, "default: {");
         return Case<indent + 1, Last>({{buffer}});
     }
 
     INLINE auto end () {
-        ADD(indent - 1, "}");
+        LINE(indent - 1, "}");
         return c_cast<Last>(__CodeBlock<indent - 1, typename Last::__Last, typename Last::__Derived>({this->buffer}));
     }
 };
 template <size_t indent, typename Last>
 INLINE auto Case<indent, Last>::end ()  {
-    ADD(indent - 1, "}");
+    LINE(indent - 1, "}");
     return Switch<indent - 1, Last>({this->buffer});
 }
 template <size_t indent, typename Last, typename Derived>
 INLINE auto __CodeBlock<indent, Last, Derived>::_switch (std::string key) {
-    ADD(indent, "switch (" + key + ") {");
+    LINE(indent, "switch (" + key + ") {");
     return Switch<indent + 1, Derived>({this->buffer});
 };
 
@@ -166,41 +209,41 @@ struct __Struct : CodeData {
 
     private:
     INLINE auto __method (std::string method_qualifier) {
-        ADD(indent, method_qualifier);
+        LINE(indent, method_qualifier);
         return Method<indent + 1, Derived>({{buffer}});
     }
     INLINE Derived __field (std::string field_qualifier) {
-        ADD(indent, field_qualifier);
+        LINE(indent, field_qualifier);
         return c_cast<Derived>(__Struct<indent, Last, Derived>({this->buffer}));
     }
 };
 template <size_t indent, typename Last>
 INLINE auto Method<indent, Last>::end ()  {
-    ADD(indent - 1, "}");
+    LINE(indent - 1, "}");
     return c_cast<Last>(__Struct<indent - 1, typename Last::__Last, typename Last::__Derived>({this->buffer}));
 }
 
 template <size_t indent, typename Last>
 struct Struct : __Struct<indent, Last, Struct<indent, Last>> {
     INLINE auto end () {
-        ADD(indent - 1, "};");
+        LINE(indent - 1, "};");
         return c_cast<Last>(__CodeBlock<indent - 1, typename Last::__Last, typename Last::__Derived>({this->buffer}));
     }
 };
 template <size_t indent, typename Last, typename Derived>
 INLINE auto __CodeBlock<indent, Last, Derived>::_struct (std::string name) {
-    ADD(indent, "struct " + name + " {");
+    LINE(indent, "struct " + name + " {");
     return Struct<indent + 1, Derived>({this->buffer});
 };
 template <size_t indent, typename Last>
 struct NestedStruct : __Struct<indent, Last, NestedStruct<indent, Last>> {
     INLINE auto end () {
-        ADD(indent - 1, "};");
+        LINE(indent - 1, "};");
         return __end();
     }
 
     INLINE auto end (std::string name) {
-        ADD(indent - 1, "} " + name + ";");
+        LINE(indent - 1, "} " + name + ";");
         return __end();
     }
 
@@ -211,12 +254,12 @@ struct NestedStruct : __Struct<indent, Last, NestedStruct<indent, Last>> {
 };
 template <size_t indent, typename Last, typename Derived>
 INLINE auto __Struct<indent, Last, Derived>::_struct (std::string name) {
-    ADD(indent, "struct " + name + " {");
+    LINE(indent, "struct " + name + " {");
     return NestedStruct<indent + 1, Derived>({this->buffer});
 }
 template <size_t indent, typename Last, typename Derived>
 INLINE auto __Struct<indent, Last, Derived>::_struct (std::string attributes, std::string name) {
-    ADD(indent, attributes + " struct " + name + " {");
+    LINE(indent, attributes + " struct " + name + " {");
     return NestedStruct<indent + 1, Derived>({this->buffer});
 }
 
@@ -229,20 +272,22 @@ INLINE void test () {
     for (size_t i = 0; i < 5000000; i++) {
         uint8_t mem[5000];
         auto res = create_code(mem)
-            .add("aawdw")
+            ("aawdw").nl()
+            ("a + ")("b").nl()
+
             ._struct("naur")
                 ._struct("const", "a")
                     .field("int", "a")
                 .end()
                 
                 .method("void", "a")
-                    .add("aawdw")
+                    ("aawdw").nl()
                     ._if("true")
                     .end()
                 .end()
             .end()
             ._if("true")
-                .add("aawdw")
+                ("aawdw").nl()
                 ._if("true")
                 ._else()
                 .end()
@@ -258,7 +303,7 @@ INLINE void test () {
                 
             .end()
         .end().c_str();
-        printf("%s\n", res);
+        //printf("%s\n", res);
     }
 }
 
