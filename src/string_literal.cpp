fa2dd4b1dfe8e4b2a2f1755b77e2bc9d62245e31
huben1337 <88612147+huben1337@users.noreply.github.com>
2025-01-31 20:07:24 +0100

    New capabilities  + refactoring

--- src/string_literal.cpp
+++ src/string_literal.cpp
@@ -1,21 +1,47 @@
 #pragma once
 
 #include <stdint.h>
-#include <algorithm>
+#include <utility>
+#include <array>
+
+#include "constexpr_helpers.cpp"
 
 template<size_t N>
 struct StringLiteral {
-    constexpr StringLiteral(const char (&str)[N]) {
-        std::copy_n(str, N, value);
-    }
-    
-    char value[N];
+    constexpr StringLiteral(const char (&str)[N])
+        : value(to_array(str))
+    {}
+    constexpr StringLiteral(const char _char)
+        : value({_char, '\0'})
+    {}
+    constexpr StringLiteral(const std::array<const char, N> value) : value(value) {}
+    // constexpr StringLiteral(const std::array<char, N> value) : value(static_cast<std::array<const char, N>>(value)) {}
+        
+    const std::array<const char, N> value;
+    constexpr size_t size () const { return N - 1; }
 
-    constexpr char *ptr() const {
-        return (char*)(value);
+    constexpr const char *c_str() const {
+        return value.data();
+    }
+
+    constexpr const char *data() const {
+        return value.data();
+    }
+
+    template <size_t M>
+    constexpr StringLiteral<N + M - 1> operator + (const StringLiteral<M>& other) const {
+        return StringLiteral<N + M - 1>(concat(pop_back(value), other.value));
     }
 };
 
+template <typename T>
+constexpr auto to_constexpr (T t) { return t; }
+
+
+template<StringLiteral str>
+constexpr auto operator ""_sl () {
+    return str;
+}
 /*
 template <const std::string& str>
 constexpr auto make_string_literal () {
