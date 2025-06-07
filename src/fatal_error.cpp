769852627ef036c95fac9c7138663dfa58575cbf
huben1337 <88612147+huben1337@users.noreply.github.com>
2025-06-07 14:43:16 +0200

    make fatal error logging not buffered

--- src/fatal_error.cpp
+++ src/fatal_error.cpp
@@ -4,4 +4,4 @@
 #include <cstddef>
 #include <cstdlib>
 
-#define INTERNAL_ERROR(MSG, MORE...) logger::error<MSG>(MORE); exit(1);
+#define INTERNAL_ERROR(MSG, MORE...) logger::error<false, MSG>(MORE); exit(1);
