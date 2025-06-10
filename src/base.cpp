ccfddaa20ddb1861e9769ce31e484a039b9c428c
huben1337 <88612147+huben1337@users.noreply.github.com>
2025-06-10 20:43:24 +0200

    Add __MINGW__ and fix static_warn

--- src/base.cpp
+++ src/base.cpp
@@ -2,10 +2,15 @@
 
 #define INLINE inline
 
-#define static_warn(msg)                                        \
-{                                                               \
-    struct Warning {                                            \
-        [[gnu::warning(msg)]] static constexpr void warn () {}  \
-    };                                                          \
-    Warning::warn();                                            \
+#define static_warn(msg)                                                                                \
+{                                                                                                       \
+    struct Warning {                                                                                    \
+        [[gnu::noinline]] [[clang::noinline]] [[gnu::warning(msg)]] static constexpr void warn () {}    \
+    };                                                                                                  \
+    Warning::warn();                                                                                    \
 }
+
+
+#if defined (__MINGW32__) || defined (__MINGW64__)
+#define __MINGW__ 1
+#endif
