3479a0e2501aee7ac810248e1c18acce4151846b
huben1337 <88612147+huben1337@users.noreply.github.com>
2025-06-17 20:07:01 +0200

    Change some functions to consteval as they shouldnt be used at runtime + constexpr exponentiation stuff

--- src/constexpr_helpers.cpp
+++ src/constexpr_helpers.cpp
@@ -5,7 +5,7 @@
 #include <utility>
 
 template <typename T, size_t N>
-constexpr std::array<T, N> to_array(T (&data)[N]) {
+consteval std::array<T, N> to_array(T (&data)[N]) {
     constexpr auto _to_array = []<size_t... Indices>(T (&data)[N], std::index_sequence<Indices...>)->std::array<T, N> {
         return {data[Indices]...};
     };
@@ -13,7 +13,7 @@ constexpr std::array<T, N> to_array(T (&data)[N]) {
 }
 
 template <typename T, size_t N>
-constexpr std::array<T, N + 1> push_back(std::array<T, N> arr, std::remove_const_t<T> value) {
+consteval std::array<T, N + 1> push_back(std::array<T, N> arr, std::remove_const_t<T> value) {
     constexpr auto _push_back = []<size_t... Indices>(std::array<T, N> arr, std::remove_const_t<T> value, std::index_sequence<Indices...>)->std::array<T, N + 1> {
         return {arr[Indices]..., value};
     };
@@ -21,7 +21,7 @@ constexpr std::array<T, N + 1> push_back(std::array<T, N> arr, std::remove_const
 }
 
 template <typename T, size_t N>
-constexpr std::array<T, N + 1> push_front(std::array<T, N> arr, std::remove_const_t<T> value) {
+consteval std::array<T, N + 1> push_front(std::array<T, N> arr, std::remove_const_t<T> value) {
     constexpr auto _push_front = []<size_t... Indices>(std::array<T, N> arr, std::remove_const_t<T> value, std::index_sequence<Indices...>)->std::array<T, N + 1> {
         return {value, arr[Indices]...};
     };
@@ -30,7 +30,7 @@ constexpr std::array<T, N + 1> push_front(std::array<T, N> arr, std::remove_cons
 
 template <typename T, size_t N>
 requires (N > 0)
-constexpr std::array<T, N - 1> pop_back(std::array<T, N> arr) {
+consteval std::array<T, N - 1> pop_back(std::array<T, N> arr) {
     constexpr auto _pop_back = []<size_t... Indices>(std::array<T, N> arr, std::index_sequence<Indices...>)->std::array<T, N - 1> {
         return {arr[Indices]...};
     };
@@ -39,7 +39,7 @@ constexpr std::array<T, N - 1> pop_back(std::array<T, N> arr) {
 
 template <typename T, size_t N>
 requires (N > 0)
-constexpr std::array<T, N - 1> pop_front(std::array<T, N> arr) {
+consteval std::array<T, N - 1> pop_front(std::array<T, N> arr) {
     constexpr auto _pop_front = []<size_t... Indices>(std::array<T, N> arr, std::index_sequence<Indices...>)->std::array<T, N - 1> {
         return {arr[Indices + 1]...};
     };
@@ -47,7 +47,7 @@ constexpr std::array<T, N - 1> pop_front(std::array<T, N> arr) {
 }
 
 template <typename T, size_t N, size_t M>
-constexpr std::array<T, N + M> concat(std::array<T, N> arr1, std::array<T, M> arr2) {
+consteval std::array<T, N + M> concat(std::array<T, N> arr1, std::array<T, M> arr2) {
     constexpr auto _concat = []<size_t... Indices1, size_t... Indices2>(std::array<T, N> arr1, std::array<T, M> arr2, std::index_sequence<Indices1...>, std::index_sequence<Indices2...>)->std::array<T, N + M> {
         return {arr1[Indices1]..., arr2[Indices2]...};
     };
@@ -66,17 +66,17 @@ concept CallableWithValueTemplateAndArgs = requires(F func, Args... args) {
 
 template <typename T, T... Values, typename F>
 requires (CallableWithValueTemplate<F, Values> && ...)
-constexpr void for_(F&& func) {
+consteval void for_(F&& func) {
     (func.template operator()<Values>(), ...);
 }
 template <typename T, T... Values, typename F>
 requires (CallableWithValueTemplate<F, Values> && ...)
-constexpr void for_(F&& func, const std::integer_sequence<T, Values...>) {
+consteval void for_(F&& func, const std::integer_sequence<T, Values...>) {
     (func.template operator()<Values>(), ...);
 }
 
 template<std::size_t N, std::size_t... Seq>
-constexpr std::index_sequence<N + Seq ...> add(std::index_sequence<Seq...>) { return {}; }
+consteval std::index_sequence<N + Seq ...> add(std::index_sequence<Seq...>) { return {}; }
 
 template<std::size_t Min, std::size_t Max>
 using make_index_range = decltype(add<Min>(std::make_index_sequence<Max-Min>()));
@@ -89,3 +89,21 @@ constexpr size_t uint_log10 = uint_log<10, value>;
 
 template <size_t value>
 constexpr size_t uint_log2 = uint_log<2, value>;
+
+template <size_t base, size_t exponent>
+consteval size_t _uint_pow () {
+    if constexpr (exponent == 0) {
+        return 1;
+    } else {
+        return base * _uint_pow<base, exponent - 1>();
+    }
+}
+
+template <size_t base, size_t exponent>
+constexpr size_t uint_pow = _uint_pow<base, exponent>();
+
+template <uint64_t value, uint64_t base>
+constexpr bool is_power_of = uint_pow<base, uint_log<base, value>> == value;
+
+template <uint64_t value, uint64_t base>
+constexpr bool is_power_of_with_non_zero_exponent = value != 1 && is_power_of<value, base>;
