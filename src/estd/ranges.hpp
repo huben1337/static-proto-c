68985d0b606bdd9d19618eace57852656abf89c8
huben1337 <88612147+huben1337@users.noreply.github.com>
2025-12-16 16:58:34 +0100

    Make integral_range more correct

--- src/estd/ranges.hpp
+++ src/estd/ranges.hpp
@@ -1,24 +1,54 @@
 #pragma once
 
 #include <concepts>
+#include <gsl/util>
+#include <type_traits>
 
 namespace estd {
+    template <std::unsigned_integral T>
+    struct integral_range_size {
+        template <std::integral>
+        friend struct integral_range;
+    private:
+        T value;
+    public:
+        explicit constexpr integral_range_size (T value)
+            : value(value) {}
+    };
+    
     template <std::integral T>
     struct integral_range {
-        constexpr integral_range () = default;
-        constexpr integral_range (T from, T to) : from(from), to(to) {}
+        using unsigned_type = std::make_unsigned_t<T>;
 
+    private:
         T from;
         T to;
 
+    public:
+        constexpr integral_range () = default;
+        constexpr integral_range (T from, T to) : from(from), to(to) {}
+        constexpr integral_range (T from, integral_range_size<unsigned_type> size) : from(from), to(from + size.value) {}
+
+
+
         struct iterator {
+        private:
             T pos;
-            const iterator& operator ++ () { ++pos; return *this; }
+        public:
+            constexpr explicit iterator (T pos) : pos(pos) {}
+
+            iterator& operator ++ () { ++pos; return *this; }
             bool operator == (const iterator& other) const { return pos == other.pos; }
-            T& operator * () { return pos; }
+            const T& operator * () const { return pos; }
         };
 
-        [[nodiscard]] iterator begin () const { return {from}; }
-        [[nodiscard]] iterator end () const { return {to}; }
+        [[nodiscard]] unsigned_type size () const { return gsl::narrow_cast<unsigned_type>(to - from); }
+
+        [[nodiscard]] integral_range_size<unsigned_type> wrapped_size () const { 
+            return integral_range_size<unsigned_type>{size()};
+        }
+
+        [[nodiscard]] iterator begin () const { return iterator{from}; }
+        [[nodiscard]] iterator end () const { return iterator{to}; }
     };
 }
