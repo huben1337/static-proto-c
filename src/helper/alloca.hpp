98ca496b9e3d7eb51e9313e0954bc3b28e9d0b15
huben1337 <88612147+huben1337@users.noreply.github.com>
2025-12-16 16:59:57 +0100

    Add macro to wrap stack allocation in std::span

--- src/helper/alloca.hpp
+++ src/helper/alloca.hpp
@@ -1,10 +1,24 @@
 #pragma once
 
-#define ALLOCA(TYPE, LENGTH) static_cast<TYPE*>(__builtin_alloca_with_align((LENGTH) * sizeof(TYPE), alignof(TYPE) * 8))
+#define ALLOCA(TYPE, LENGTH) \
+static_cast<TYPE*>(__builtin_alloca_with_align((LENGTH) * sizeof(TYPE), alignof(TYPE) * 8))
 
 #define ALLOCA_SAFE(NAME, TYPE, LENGTH) TYPE* NAME;         \
-if (LENGTH > 0) {                                           \
-    NAME = ALLOCA(TYPE, LENGTH);                            \
+if (size_t length = LENGTH; length > 0) {                   \
+    NAME = ALLOCA(TYPE, length);                            \
 } else {                                                    \
     NAME = nullptr;                                         \
+}
+
+#define ALLOCA_UNSAFE_SPAN(NAME, TYPE, LENGTH)                      \
+std::span<TYPE> NAME;                                               \
+{                                                                   \
+    const size_t length = LENGTH;                                   \
+    NAME = std::span<TYPE>{ALLOCA(TYPE, length), length};           \
+}
+
+#define ALLOCA_SAFE_SPAN(NAME, TYPE, LENGTH)                        \
+std::span<TYPE> NAME;                                               \
+if (const size_t length = LENGTH; length > 0) {                     \
+    NAME = std::span<TYPE>{ALLOCA(TYPE, length), length};           \
 }
