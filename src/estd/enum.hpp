80f8ed7897180fe0870178f4eec3b708a2b15ead
huben1337 <88612147+huben1337@users.noreply.github.com>
2025-12-16 16:56:19 +0100

    Make enum loggable

--- src/estd/enum.hpp
+++ src/estd/enum.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
+#include <concepts>
+
+#include "../util/string_literal.hpp"
+#include "../util/logger.hpp"
+
 namespace estd {
 
-template <typename T>
+namespace {
+
+    template <typename Outside>
+    constexpr StringLiteral enum_class_outside_name = string_literal::from_([](){ return nameof::nameof_type<Outside>(); });
+
+    template <>
+    constexpr StringLiteral enum_class_outside_name<void> = "ENUM_CLASS<>";
+}
+
+template <std::integral T, typename Outside = void>
 struct ENUM_CLASS {
 public:
     using value_t = T;
@@ -20,6 +34,12 @@ public:
     [[nodiscard]] constexpr operator value_t () const { return value; }
 
     [[nodiscard]] explicit operator bool () const = delete;
+
+    template <typename writer_params>
+    void log (logger::writer<writer_params> w) const {
+        w.template write<true, true>(enum_class_outside_name<Outside> + "{value: "_sl, value, "}");
+    }
+
 protected:
     constexpr explicit ENUM_CLASS (const value_t value) : value(value) {}
     constexpr ~ENUM_CLASS () = default;
