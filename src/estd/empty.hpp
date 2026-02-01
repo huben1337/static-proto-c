fbf9d13f48dcb020370ba653b42506ffc344fb25
huben1337 <88612147+huben1337@users.noreply.github.com>
2026-02-01 13:24:48 +0100

    Add dummy operators

--- src/estd/empty.hpp
+++ src/estd/empty.hpp
@@ -5,11 +5,58 @@ namespace estd {
     struct empty {
         consteval empty () = default;
         consteval empty (const empty& /*unused*/) = default;
-        constexpr empty (empty&& /*unused*/)  noexcept {}
+        constexpr empty (empty&& /*unused*/) = default;
 
-        empty& operator = (const empty&) = delete;
-        empty& operator = (empty&&) = delete;
+        consteval empty& operator = (const empty&) = default;
+        consteval empty& operator = (empty&&) = default;
 
         constexpr ~empty () = default;
+
+        consteval empty operator*() const noexcept { return {}; }
+        // consteval const empty* operator->() const noexcept { return this; }
+
+        // comparison - disabled since i can't find a sensible way to do this
+        // consteval bool operator==(empty /*unused*/) const { return true; }
+        // consteval bool operator!=(empty /*unused*/) const { return false; }
+        // consteval bool operator< (empty /*unused*/) const { return false; }
+        // consteval bool operator<=(empty /*unused*/) const { return true; }
+        // consteval bool operator> (empty /*unused*/) const { return false; }
+        // consteval bool operator>=(empty /*unused*/) const { return true; }
+
+        // unary
+        consteval empty operator+() const { return {}; }
+        consteval empty operator-() const { return {}; }
+
+        // inc / dec
+        consteval empty operator++()    const { return {}; }
+        consteval empty operator++(int) const { return {}; }
+        consteval empty operator--()    const { return {}; }
+        consteval empty operator--(int) const { return {}; }
+
+        // arithmetic
+        consteval empty operator+(empty /*unused*/) const { return {}; }
+        consteval empty operator-(empty /*unused*/) const { return {}; }
+        consteval empty operator*(empty /*unused*/) const { return {}; }
+        consteval empty operator/(empty /*unused*/) const { return {}; }
+        consteval empty operator%(empty /*unused*/) const { return {}; }
+
+        // bitwise
+        consteval empty operator &(empty /*unused*/) const { return {}; }
+        consteval empty operator |(empty /*unused*/) const { return {}; }
+        consteval empty operator ^(empty /*unused*/) const { return {}; }
+        consteval empty operator<<(empty /*unused*/) const { return {}; }
+        consteval empty operator>>(empty /*unused*/) const { return {}; }
+
+        // compound assignment
+        consteval empty operator +=(empty /*unused*/) const { return {}; }
+        consteval empty operator -=(empty /*unused*/) const { return {}; }
+        consteval empty operator *=(empty /*unused*/) const { return {}; }
+        consteval empty operator /=(empty /*unused*/) const { return {}; }
+        consteval empty operator %=(empty /*unused*/) const { return {}; }
+        consteval empty operator &=(empty /*unused*/) const { return {}; }
+        consteval empty operator |=(empty /*unused*/) const { return {}; }
+        consteval empty operator ^=(empty /*unused*/) const { return {}; }
+        consteval empty operator<<=(empty /*unused*/) const { return {}; }
+        consteval empty operator>>=(empty /*unused*/) const { return {}; }
     };
 }
