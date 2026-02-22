3394960ac795f17968e31ee4fa75f4f51c279aa4
huben1337 <88612147+huben1337@users.noreply.github.com>
2026-02-22 13:26:41 +0100

    Add conceptify

--- src/estd/concepts.hpp
+++ src/estd/concepts.hpp
@@ -7,4 +7,7 @@ namespace estd {
 
     template <typename FuncT, template <typename> typename RetTT, typename... ArgsT>
     concept invocable_r = RetTT<std::invoke_result_t<FuncT, ArgsT...>>::value;
+
+    template <typename T, template <typename> typename TT>
+    concept conceptify = TT<T>::value;
 }
