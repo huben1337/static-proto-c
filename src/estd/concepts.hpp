961f25fa105c559a16450687262d8ce889146e16
huben1337 <88612147+huben1337@users.noreply.github.com>
2026-03-18 23:15:41 +0100

    Add different_from concept

--- src/estd/concepts.hpp
+++ src/estd/concepts.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <type_traits>
+#include <concepts>
 
 
 namespace estd {
@@ -10,4 +11,7 @@ namespace estd {
 
     template <typename T, template <typename> typename TT>
     concept conceptify = TT<T>::value;
+
+    template <typename T, typename U>
+    concept different_from = !std::same_as<std::remove_cvref_t<T>, std::remove_cvref_t<U>>;
 }
