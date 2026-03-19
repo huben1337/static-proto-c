ee1ef804395d2d2746f885e23f8ae642f2cff699
huben1337 <88612147+huben1337@users.noreply.github.com>
2026-03-19 22:07:00 +0100

    Refactor, Add are_distinct_variadic_v

--- src/estd/utility.hpp
+++ src/estd/utility.hpp
@@ -21,7 +21,7 @@ namespace estd {
         return std::forward<U>(u);
     }
 
-    namespace {
+    namespace _detail {
         template <typename To, typename From>
         consteval void check_casting_const_correctness_ () {
             if constexpr (std::is_const_v<std::remove_reference_t<From>>) {
@@ -33,7 +33,7 @@ namespace estd {
     template <typename To, typename From>
     [[nodiscard, gnu::always_inline]] constexpr To* ptr_cast (From* from) {
         static_assert(alignof(From) >= alignof(To), "Can't cast pointer to type of lower alignment");
-        check_casting_const_correctness_<To, From>();
+        _detail::check_casting_const_correctness_<To, From>();
         return reinterpret_cast<To*>(from);
     }
 
@@ -43,7 +43,7 @@ namespace estd {
             "Siblings must be layout compatible");
         if constexpr (std::is_lvalue_reference_v<To>) {
             static_assert(std::is_lvalue_reference_v<From>, "Can not cast from rvalue to lvalue");
-            check_casting_const_correctness_<To, From>();
+            _detail::check_casting_const_correctness_<To, From>();
         } else {
             if constexpr (!std::is_trivially_copyable_v<std::remove_reference_t<From>>) {
                 static_assert(!std::is_reference_v<From>,
@@ -56,7 +56,7 @@ namespace estd {
     }
 
     template <auto v>
-    struct value_t {
+    struct constant {
         static constexpr auto value = v;
     };
 
@@ -64,7 +64,7 @@ namespace estd {
     using nth_t = std::_Nth_type<N, T...>::type;
 
     template <size_t N, auto... v>
-    constexpr auto nth_v = std::_Nth_type<N, value_t<v>...>::type::value;
+    constexpr auto nth_v = std::_Nth_type<N, constant<v>...>::type::value;
 
     template <auto first, auto... rest>
     constexpr bool are_distinct_v = ((first != rest) && ...) && are_distinct_v<rest...>;
@@ -86,13 +86,13 @@ namespace estd {
         using append = variadic_t<T..., U...>;
 
         template <template <typename> typename MappedType>
-        using map = estd::variadic_t<typename MappedType<T>::type...>;
+        using map = variadic_t<typename MappedType<T>::type...>;
 
         template <template <typename...> typename Template>
         using apply = Template<T...>;
 
         template <size_t N>
-        using nth_t = estd::nth_t<N, T...>;
+        using nth_t = nth_t<N, T...>;
 
         static constexpr size_t size = sizeof...(T);
 
@@ -101,6 +101,7 @@ namespace estd {
             (std::forward<F>(lambda).template operator()<T>(args...), ...);
         }
     };
+
 
     template<typename... T>
     struct reverse_variadic_t;
@@ -118,6 +119,11 @@ namespace estd {
     template<typename... T>
     using reverse_variadic_t_t = reverse_variadic_t<T...>::type;
 
+
+    template <auto... v>
+    using transform_to_varidadic_t = variadic_t<constant<v>...>;
+    
+
     template <auto... v>
     struct variadic_v {
     private:
@@ -132,7 +138,7 @@ namespace estd {
         using append = variadic_v<v..., w...>;
 
         template <template <auto> typename MappedValue>
-        using map = estd::variadic_v<MappedValue<v>::value...>;
+        using map = variadic_v<MappedValue<v>::value...>;
 
         template <template <auto...> typename Template>
         using apply = Template<v...>;
@@ -147,6 +153,7 @@ namespace estd {
             (std::forward<F>(lambda).template operator()<v>(args...), ...);
         }
     };
+
 
     template<auto... v>
     struct reverse_variadic_v;
@@ -163,6 +170,32 @@ namespace estd {
 
     template<auto... v>
     using reverse_variadic_v_t = reverse_variadic_v<v...>::type;
+
+    namespace _detail {
+        template <auto lhs, auto rhs>
+        struct values_equal {
+            static constexpr bool value = lhs == rhs;
+        };
+    }
+
+    template <template <auto, auto> typename AreSame = _detail::values_equal>
+    struct are_distinct_variadic_v {
+        template<auto... v>
+        struct check;
+
+        template<>
+        struct check<> {
+            static constexpr bool value = true;
+        };
+
+        template<auto first, auto... rest>
+        struct check<first, rest...> {
+            static constexpr bool value = (!AreSame<first, rest>::value && ...) && check<rest...>::value;
+        };
+    };
+
+    template <typename... T>
+    using transform_to_varidadic_v = variadic_v<T::value...>;
 
 
     namespace {
@@ -195,7 +228,7 @@ namespace estd {
 
         template <size_t N, typename TargetT, typename FirstT>
         struct variadic_type_index_<N, TargetT, FirstT> {
-            static_assert(false, "Invalid variant type.");
+            static_assert(false, "TargetT not found in variadic type pack");
         };
 
         template <size_t N, typename TargetT, typename FirstT>
