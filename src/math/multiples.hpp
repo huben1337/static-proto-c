1f662dd360780276e53a0fb2b2d27f1bf1ea2740
huben1337 <88612147+huben1337@users.noreply.github.com>
2026-07-09 23:49:52 +0200

    Add last_mutiple and assertions

--- src/math/multiples.hpp
+++ src/math/multiples.hpp
@@ -1,36 +1,65 @@
 #pragma once
 
+#include <bit>
 #include <concepts>
+#include <cstddef>
 
-#include "../base.hpp"
 #include "../estd/utility.hpp"
 #include "../core/SIZE.hpp"
 
 namespace math {
 
+namespace detail {
+    template <std::integral T>
+    [[nodiscard, gnu::always_inline]] constexpr T last_multiple (T value, T base) {
+        return value & ~(base - 1);
+    }
+
+    template <std::integral T>
+    [[nodiscard, gnu::always_inline]] constexpr T next_multiple (T value, T base) {
+        T mask = base - 1;
+        return (value + mask) & ~mask;
+    }
+} // namespace detail
+
 template <std::integral T>
 [[nodiscard]] constexpr T last_multiple (T value, SIZE base) {
-    return value & ~(static_cast<T>(base.byte_size()) - 1);
+    return detail::last_multiple<T>(value, base.byte_size());
+}
+
+
+template <std::integral T, size_t base>
+[[nodiscard]] constexpr T last_multiple (T value) {
+    static_assert(std::has_single_bit(base), "base must be a power of 2");
+
+    return detail::last_multiple<T>(value, base);
 }
 
 template <std::integral T, estd::discouraged_annotation>
 [[nodiscard]] constexpr T last_multiple (T value, std::type_identity_t<T> base) {
-    static_warn("base must be a power of 2");
-    return value & ~(base - 1);
+    assert(std::has_single_bit(base));
+
+    return detail::last_multiple<T>(value, base);
 }
 
 
 template <std::integral T>
 [[nodiscard]] constexpr T next_multiple (T value, SIZE base) {
-    T mask = static_cast<T>(base.byte_size()) - 1;
-    return (value + mask) & ~mask;
+    return detail::next_multiple<T>(value, base.byte_size());
+}
+
+template <std::integral T, size_t base>
+[[nodiscard]] constexpr T next_multiple (T value) {
+    static_assert(std::has_single_bit(base), "base must be a power of 2");
+    
+    return detail::next_multiple<T>(value, base);
 }
 
 template <std::integral T, estd::discouraged_annotation>
 [[nodiscard]] constexpr T next_multiple (T value, std::type_identity_t<T> base) {
-    static_warn("base must be a power of 2");
-    T mask = base - 1;
-    return (value + mask) & ~mask;
+    assert(std::has_single_bit(base));
+
+    return detail::next_multiple<T>(value, base);
 }
 
 } // namespace math
