fc46996a429792dc29ff02861bed3928f5dc8573
huben1337 <88612147+huben1337@users.noreply.github.com>
2026-07-09 23:36:26 +0200

    Correct accessing of iterables

--- src/estd/ranges.hpp
+++ src/estd/ranges.hpp
@@ -9,6 +9,13 @@
 #include <type_traits>
 
 namespace estd {
+    namespace detail {
+        template <typename R>
+        concept SizedContiguousRange =
+            std::ranges::contiguous_range<R> &&
+            std::ranges::sized_range<R>;
+    } // namespace detail
+
     template <std::unsigned_integral T>
     struct integral_range_size {
         template <std::integral>
@@ -19,7 +26,7 @@ namespace estd {
         explicit constexpr integral_range_size (T value)
             : value(value) {}
     };
-    
+
     template <std::integral T>
     struct integral_range {
         using unsigned_type = std::make_unsigned_t<T>;
@@ -30,10 +37,12 @@ namespace estd {
 
     public:
         consteval integral_range () = default;
-        constexpr integral_range (T from, T to) : from(from), to(to) {}
+
+        constexpr integral_range (T from, T to) : from(from), to(to) {
+            assert(from <= to);
+        }
+        
         constexpr integral_range (T from, integral_range_size<unsigned_type> size) : from(from), to(from + size.value) {}
-
-
 
         struct iterator {
         private:
@@ -61,34 +70,21 @@ namespace estd {
         template <typename R>
         using iterator_from_begin_t = decltype(std::declval<R&>().begin());
 
-        template <typename Iterable, typename With>
-        [[nodiscard]] constexpr With access_iterable (Iterable& i) const {
-            auto begin = i.begin();
-            auto accessed_end = begin + to;
-            assert(accessed_end <= i.end());
-            return With{
-                begin + from,
-                accessed_end
-            };
-        }
-
-        template <typename U, typename With>
-        [[nodiscard]] constexpr With access_ptr (U* const p) const {
-            assert(p != nullptr);
-            return With{
-                p + from,
-                p + to
-            };
-        }
-
     public:
         template <
             typename Iterable,
             std::same_as<template_guard> = template_guard,
-            typename With = std::span<std::iter_reference_t<iterator_from_begin_t<Iterable>>>
+            typename With = std::span<std::ranges::range_value_t<Iterable>>
         >
-        [[nodiscard]] constexpr With access_subspan (Iterable& i) const {
-            return access_iterable<Iterable, With>(i);
+        [[nodiscard]] constexpr With access_subspan (Iterable& i) const {    
+            using iterable_difference_t = std::ranges::range_difference_t<Iterable>;
+
+            assert(to <= std::ranges::size(i));
+
+            return With{
+                i.begin() + gsl::narrow_cast<iterable_difference_t>(from),
+                size()
+            };
         }
 
         template <
@@ -97,16 +93,30 @@ namespace estd {
             typename With = std::span<U>
         >
         [[nodiscard]] constexpr With access_subspan (U* const p) const {
-            return access_ptr<U, With>(p);
+            assert(p != nullptr);
+
+            return With{
+                p + from,
+                size()
+            };
         }
 
         template <
             typename Iterable,
             std::same_as<template_guard> = template_guard,
-            typename With = std::ranges::subrange<iterator_from_begin_t<Iterable>>
+            typename With = std::ranges::subrange<std::ranges::iterator_t<Iterable>>
         >
         [[nodiscard]] constexpr With access_subrange (Iterable& i) const {
-            return access_iterable<Iterable, With>(i);
+            using iterable_difference_t = std::ranges::range_difference_t<Iterable>;
+            
+            assert(to <= std::ranges::size(i));
+
+            auto begin = i.begin();
+
+            return With{
+                begin + gsl::narrow_cast<iterable_difference_t>(from),
+                begin + gsl::narrow_cast<iterable_difference_t>(to)
+            };
         }
 
         template <
@@ -115,7 +125,12 @@ namespace estd {
             typename With = std::ranges::subrange<U*>
         >
         [[nodiscard]] constexpr With access_subrange (U* const p) const {
-            return access_ptr<U, With>(p);
+            assert(p != nullptr);
+
+            return With{
+                p + from,
+                p + to
+            };
         }
     };
 
